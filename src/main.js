85fd9d327b786c0f730402cadbf4528ad05a25e3
THR <tanghran@126.com>
2021-02-25 07:30:41 +0800

    初次提交

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+createApp(App).use(router).use(ElementUI).mount('#app')
